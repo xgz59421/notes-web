4dc28404c0ae1d209f3aa4a740f581a3026e09a3
zhanghao <87256657@qq.com>
2020-12-08 21:29:06 +0800

    formate

--- 05_http_ajax/test/03_$ajax/07_corse/server-agent/server-agent.js
+++ 05_http_ajax/test/03_$ajax/07_corse/server-agent/server-agent.js
@@ -1,16 +1,16 @@
 const http = require("http");
 http.createServer(function (req, res) {
-   res.writeHead(200, {
-      "Content-Type": "text/plain;charset=utf8",
-      //解决跨域
-      // "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
-      // "Access-Control-Allow-Origin": "http://127.0.0.1:5501",
-      //或
-      // "Access-Control-Allow-Origin": "*",
+  res.writeHead(200, {
+    "Content-Type": "text/plain;charset=utf8",
+    //解决跨域
+    // "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
+    // "Access-Control-Allow-Origin": "http://127.0.0.1:5501",
+    //或
+    // "Access-Control-Allow-Origin": "*",
 
-   });
-   res.write("北京 晴 38°");
-   res.end();
+  });
+  res.write("北京 晴 38°");
+  res.end();
 
 }).listen(3000);
 console.log('启动了服务器');
