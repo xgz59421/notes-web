f70c745a575754c4c3475127f33d982d61b48c41
zhanghao <87256657@qq.com>
2021-05-16 15:58:04 +0800

    add demo

--- 02_js_lagou/03-webpack/07-eslint/07-elint-webpack/zce-react-app/webpack.config.js
+++ 02_js_lagou/03-webpack/07-eslint/07-elint-webpack/zce-react-app/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         use: 'babel-loader'
       },
       {
+        // 添加es-loader
         test: /\.js$/, 
         exclude: /node_modules/, 
         use: 'eslint-loader',
@@ -32,9 +33,12 @@ module.exports = {
   ]
 }
 
+// https://github.com/zce/zce-react-app.git
 // 安装 eslint 模块
 // yarn add eslint --dev
 // 安装 eslint-loader 模块
 // yarn add eslint-loader --dev
 // 初始化 .eslintrc.js 配置文件
 // yarn eslint --init
+
+// yarn webpack
