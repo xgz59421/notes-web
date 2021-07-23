13b8eb9f5d27d0b983e0d143b912ba85291a38eb
zh <87256657@qq.com>
2021-07-23 17:02:23 +0800

    mdf

--- 02-js/01-javascript/01-数据类型和运算符/01-数据类型.js
+++ 02-js/01-javascript/01-数据类型和运算符/01-数据类型.js
@@ -1,6 +1,27 @@
-//基本数据类型: number,string,bool,undefined,null
+//基本数据类型: number,string,bool,undefined,null, symbol
 //引用数据类型: array,object,function
 
+// 判断方法 
+// 1. typeof
+console.log(typeof null); // 注意 typeof null === 'object'
+// console.log(typeof null === 'object');
+
+// 2. instanceof 判断是否是某个类的实例
+console.log({} instanceof Object);
+
+// 3. Object.prototype.toString
+console.log(Object.prototype.toString.call('hello'));//[object String]
+console.log(Object.prototype.toString.call(true))//[object Boolean]
+console.log(Object.prototype.toString.call(123));//[object Number]
+console.log(Object.prototype.toString.call(null));//[object Null]
+console.log(Object.prototype.toString.call(undefined));//[object Undefined]
+
+// 4. constructor
+console.log('hello'.constructor == String);
+console.log(true.constructor == Boolean);
+// console.log(undefined.constructor == Object);
+
+console.log('------------------------');
 //1.基本数据存储
 var a = 1;
 var b = a;  //使用栈内存 会copy一份数据
