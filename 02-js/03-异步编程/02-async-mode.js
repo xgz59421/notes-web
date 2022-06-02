caf24fc8b61a3c23897c184f09152bbeb78df9fa
zh <87256657@qq.com>
2022-06-02 16:34:24 +0800

    1

--- 02-js/03-异步编程/02-async-mode.js
+++ 02-js/03-异步编程/02-async-mode.js
@@ -1,15 +1,15 @@
-console.log('global begin')
+console.log('global begin')
 
-setTimeout(function timer1 () {
-  console.log('timer1 invoke')
-}, 1800)
+setTimeout(function timer1() {
+  console.log('timer1 invoke')
+}, 1800)
 
-setTimeout(function timer2 () {
-  console.log('timer2 invoke')
+setTimeout(function timer2() {
+  console.log('timer2 invoke')
 
-  setTimeout(function inner () {
-    console.log('inner invoke')
-  }, 1000)
-}, 1000)
+  setTimeout(function inner() {
+    console.log('inner invoke')
+  }, 1000)
+}, 1000)
 
-console.log('global end')
+console.log('global end')
--- 02-js/03-异步编程/README.md
+++ 02-js/03-异步编程/README.md
@@ -3,11 +3,9 @@
 ## Usage
 
 ```sh
-# install deps
-$ yarn # or npm i
+yarn or npm i
+# run  examles
+yarn webpack-dev-server 01-sync-mode.js --open
 
-# run every examles
-$ yarn webpack-dev-server <example.js>
-# e.g.
-$ yarn webpack-dev-server 01-sync-mode.js
+npx webpack-dev-server 01-sync-mode.js --open
 ```
